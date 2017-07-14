8633e2f6dc4950bae31b211db693eda6bb3fa8b6
Dmitriy Vetutnev <d.vetutnev@gmail.com>
2017-07-14 16:44:25 +0300

    Custom request\response

--- main.cpp
+++ main.cpp
@@ -17,8 +17,10 @@ struct ProgramOptions
     Mode mode;
     std::string bind_ip;
     unsigned short bind_port;
+    std::string response;
     std::string remote_ip;
     unsigned short remote_port;
+    std::string request;
     unsigned short repeat;
 };
 std::ostream& operator<< (std::ostream&, const ProgramOptions&);
@@ -45,12 +47,10 @@ int main(int argc, char* argv[])
 
     auto on_timer_requester = [socket, &options](const auto&, const auto&)
     {
-        const char message[] = "ping";
-        std::cout << get_time_pretty() << " Send request <" << message << ">" << std::endl;
-
-        auto data = std::make_unique<char[]>( sizeof(message) );
-        std::copy_n( message, sizeof(message), data.get() );
-        socket->send( options.remote_ip, options.remote_port, std::move(data), sizeof(message) );
+        std::cout << get_time_pretty() << " Send request <" << options.request << ">" << std::endl;
+        auto data = std::make_unique<char[]>( options.request.size() );
+        std::copy_n( options.request.c_str(), options.request.size(), data.get() );
+        socket->send( options.remote_ip, options.remote_port, std::move(data), static_cast<unsigned int>(options.request.size()) );
     };
 
     auto on_data_requester = [](const auto& event, const auto&)
@@ -58,16 +58,13 @@ int main(int argc, char* argv[])
         std::cout << get_time_pretty() << " Received from " << event.sender.ip << ":" << event.sender.port << ", data: " << std::string{event.data.get(), event.length} << std::endl;
     };
 
-    auto on_data_responder = [](const auto& event, auto& soc)
+    auto on_data_responder = [&options](const auto& event, auto& soc)
     {
         std::cout << get_time_pretty() << " Received from " << event.sender.ip << ":" << event.sender.port << ", data: " << std::string{event.data.get(), event.length} << std::endl;
-
-        const char message[] = "pong";
-        std::cout << get_time_pretty() << " Send respond <" << message << ">" << std::endl;
-
-        auto data = std::make_unique<char[]>( sizeof(message) );
-        std::copy_n( message, sizeof(message), data.get() );
-        soc.send( event.sender.ip, event.sender.port, std::move(data), sizeof(message) );
+        std::cout << get_time_pretty() << " Send respond <" << options.response << ">" << std::endl;
+        auto data = std::make_unique<char[]>( options.response.size() );
+        std::copy_n( options.response.c_str(), options.response.size(), data.get() );
+        soc.send( event.sender.ip, event.sender.port, std::move(data), static_cast<unsigned int>(options.response.size()) );
     };
 
 
@@ -103,16 +100,18 @@ static const std::string usage =
 R"(Ping-Pong
 
         Usage:
-         ping_pong responder [--bind_ip <bind ip>] --bind_port <bind port>
-         ping_pong requester [--bind_ip <bind ip>] --bind_port <bind port> --remote_ip <remote ip> --remote_port <remote port> [--repeat <seconds>]
+         ping_pong responder [--bind_ip <bind ip>] --bind_port <bind port> [--response <pong>]
+         ping_pong requester [--bind_ip <bind ip>] --bind_port <bind port> --remote_ip <remote ip> --remote_port <remote port> [--request <ping>] [--repeat <seconds>]
          ping_pong (-h | --help)
 
         Options:
          -h --help                      Show this message
-         --bind_port <bind port>        Number port for pending data
          --bind_ip <bind ip>            Address for pending data [default: 0.0.0.0]
+         --bind_port <bind port>        Number port for pending data
+         --response <pong>              Response message [default: pong]
          --remote_ip <remote ip>        Address for send request
          --remote_port <remote port>    Number port for send request
+         --request <ping>               Request message [default: ping]
          --repeat <seconds>             Repeat time, in seconds [default: 5]
 )";
 
@@ -121,13 +120,20 @@ const ProgramOptions parse_program_options(int argc, char* argv[])
     auto opt = docopt::docopt(usage, {argv + 1, argv + argc} );
 
     Mode mode = ( opt["responder"].asBool() ) ? Mode::Responder : Mode::Requester;
+
     long bind_port = opt["--bind_port"].asLong();
+    std::string response = (mode == Mode::Responder) ? opt["--response"].asString() : "";
+
     std::string remote_ip = (opt["--remote_ip"]) ? opt["--remote_ip"].asString() : "";
     long remote_port = (opt["--remote_port"]) ? opt["--remote_port"].asLong() : 0;
+    std::string request = (mode == Mode::Requester) ? opt["--request"].asString() : "";
     long repeat = (mode == Mode::Requester) ? opt["--repeat"].asLong() : 0;
 
     using ushort = unsigned short;
-    return ProgramOptions{mode, opt["--bind_ip"].asString(), static_cast<ushort>(bind_port), std::move(remote_ip), static_cast<ushort>(remote_port), static_cast<ushort>(repeat)};
+    return ProgramOptions{mode,
+                opt["--bind_ip"].asString(), static_cast<ushort>(bind_port), response,
+                std::move(remote_ip), static_cast<ushort>(remote_port), request, static_cast<ushort>(repeat)
+    };
 }
 
 std::ostream& operator<< (std::ostream& os, Mode m)
@@ -147,8 +153,10 @@ std::ostream& operator<< (std::ostream& os, const ProgramOptions& po)
     os << "Mode: " << po.mode << std::endl;
     os << "Bind IP: " << po.bind_ip << std::endl;
     os << "Bind port: " << po.bind_port << std::endl;
+    os << "Response message: " << po.response << std::endl;
     os << "Remote IP: " << po.remote_ip << std::endl;
     os << "Remote port: " << po.remote_port << std::endl;
+    os << "Request message: " << po.request << std::endl;
     os << "Repeat time: " << po.repeat << std::endl;
     return os;
 }
